3ed3f769bea800719444b7dcbf7d45229b9be000
MatteoBrownlow <114264680+MatteoBrownlow@users.noreply.github.com>
2024-09-18 17:20:28 -0700

    Update assignment14.cpp

--- assignment14.cpp
+++ assignment14.cpp
@@ -7,7 +7,13 @@ int main(){
 
   long randomNumber = time(0);
   srand(randomNumber);
-  int secretNumber = rand();
-  cout << secretNumber << endl;
+  int secretNumber = (rand() % 1000) + 1;
+  cout << "Guess the number 1 - 1000" << endl;
+  int guess = 0;
+  while(guess != secretNumber){
+    cout << "Enter your guess: " << endl;
+    cin >> guess;
+  }
+  cout << "You got it! The number was " << secretNumber << endl;
   return 0;
 }
